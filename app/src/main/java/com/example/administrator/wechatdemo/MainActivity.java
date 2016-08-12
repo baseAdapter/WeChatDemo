40564202982322067f394952aceb2ce8efcae19e
dengchao <dengchao@yunhui.com>
2016-08-12 16:59:18 +0800

    .

--- app/src/main/java/com/example/administrator/wechatdemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/wechatdemo/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.administrator.wechatdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends Activity {
+    @BindView(R.id.tv_finish)
+    private TextView tv_finish;
+    @BindView(R.id.rv_img)
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    private void initView(){
+        ButterKnife.bind(this);
     }
 }
